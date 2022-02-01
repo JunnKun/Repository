8859fc08404f1238667f0cb77bfd68229b29fd1e
JunnKun <jackyhu03@gmail.com>
2022-02-01 07:44:45 +0000

    second commit

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
     $file_data = json_decode(file_get_contents("data.json"));
     foreach($file_data as $index => $value){
-        $id = $value -> dato;
+        $id = $value -> data;
         echo "[$index] $id";
     }
+    $newValore = new stdClass();
+    $newValore -> data = ($id+1);
+    $file_data[] = $newValore;
+    file_put_contents("data.json", json_encode($file_data)); 
 ?>
